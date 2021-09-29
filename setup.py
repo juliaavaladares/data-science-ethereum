98f948e22bd00040911e2fc3768f172dedc2e0eb
Júlia <julia.valadares99@gmail.com>
2021-09-29 20:58:34 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='analise de dados sobre a rede ethereum',
+    author='juliaavaladares',
+    license='BSD-3',
+)
