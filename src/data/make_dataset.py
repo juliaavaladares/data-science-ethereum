4684ca10f0deaa75bb79662b0687d937008db747
Júlia <julia.valadares99@gmail.com>
2021-10-10 22:50:04 -0300

    adiciona make_dataset para coletar novas informacoes

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,80 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+import requests
+from tqdm import tqdm
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+etherscan_token = os.getenv("TOKEN")
+etherscan_url=os.getenv("URL")
+
+#Dados
+accounts_dataset = pd.read_csv("../../data/raw/Accounts2021.csv", sep=",")
+contas = accounts_dataset["accounts"].values
+
+def write_file(line):
+    with open("new_features_request_2021.tx", "a") as file:
+        file.write(line+"\n")
+
+#Funcoes de requests
+def get_ether_balance(conta):
+    try:
+        url = etherscan_url + "?module=account&action=balance&address="+conta+"&tag=latest&apikey="+etherscan_token
+
+        response = requests.get(url)
+        result = response.json()["result"]
+        balance_ether = float(result)
+
+        return balance_ether
+
+    except Exception as ex:
+        print('Error:', ex)
+        print('Conta:', conta)
+        print('Try again')
+        get_normal_transactions(conta)
+
+def get_normal_transactions(conta):
+    try:
+        url = etherscan_url + "?module=account&action=txlist&address="+conta+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey="+etherscan_token
+
+        response = requests.get(url)
+        result = response.json()["result"]
+        number_normal_transactions = len(result)
+
+        return number_normal_transactions
+
+    except Exception as ex:
+        print('Error:', ex)
+        print('Conta:', conta)
+        print('Try again')
+        get_normal_transactions(conta)
+    
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def get_internal_transactions(conta):
+    try:
+        url = etherscan_url + "?module=account&action=txlistinternal&address="+conta+"&startblock=0&endblock=2702578&page=1&offset=10&sort=asc&apikey="+etherscan_token
 
+        response = requests.get(url)
+        result = response.json()["result"]
+        number_internal_transactions = len(result)
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+        return number_internal_transactions
+    
+    except Exception as ex:
+        print('Error:', ex)
+        print('Conta:', conta)
+        print('Try again')
+        get_internal_transactions(conta)
+    
+        
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+write_file("user_account,balance_ether,normal_transactions,internal_transactions")
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+for conta in tqdm(contas): 
+    ether, normal_transactions, internal_transactions = get_ether_balance(conta), get_normal_transactions(conta), get_internal_transactions(conta)
 
-    main()
+    linha = f"{conta},{ether},{normal_transactions},{internal_transactions}"
+    write_file(linha)
+    
