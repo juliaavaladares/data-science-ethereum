ed59ba36fd6fc360e84849f3de53091016ec1d5d
Júlia <julia.valadares99@gmail.com>
2021-10-25 19:20:59 -0300

    atualiza arquivo para a coleta de novas features

--- src/data/make_dataset_new_features.py
+++ src/data/make_dataset_new_features.py
@@ -1,80 +1,55 @@
 import pandas as pd
 import numpy as np
-import requests
-from tqdm import tqdm
-from dotenv import load_dotenv
-import os
+import tqdm 
 
-load_dotenv()
-etherscan_token = os.getenv("TOKEN")
-etherscan_url=os.getenv("URL")
-
-#Dados
-accounts_dataset = pd.read_csv("../../data/raw/Accounts2021.csv", sep=",")
-contas = accounts_dataset["accounts"].values
-
-def write_file(line):
-    with open("new_features_request_2021.tx", "a") as file:
-        file.write(line+"\n")
-
-#Funcoes de requests
-def get_ether_balance(conta):
-    try:
-        url = etherscan_url + "?module=account&action=balance&address="+conta+"&tag=latest&apikey="+etherscan_token
-
-        response = requests.get(url)
-        result = response.json()["result"]
-        balance_ether = float(result)
-
-        return balance_ether
-
-    except Exception as ex:
-        print('Error:', ex)
-        print('Conta:', conta)
-        print('Try again')
-        get_normal_transactions(conta)
-
-def get_normal_transactions(conta):
-    try:
-        url = etherscan_url + "?module=account&action=txlist&address="+conta+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey="+etherscan_token
-
-        response = requests.get(url)
-        result = response.json()["result"]
-        number_normal_transactions = len(result)
-
-        return number_normal_transactions
-
-    except Exception as ex:
-        print('Error:', ex)
-        print('Conta:', conta)
-        print('Try again')
-        get_normal_transactions(conta)
-    
-
-
-def get_internal_transactions(conta):
-    try:
-        url = etherscan_url + "?module=account&action=txlistinternal&address="+conta+"&startblock=0&endblock=2702578&page=1&offset=10&sort=asc&apikey="+etherscan_token
-
-        response = requests.get(url)
-        result = response.json()["result"]
-        number_internal_transactions = len(result)
-
-        return number_internal_transactions
-    
-    except Exception as ex:
-        print('Error:', ex)
-        print('Conta:', conta)
-        print('Try again')
-        get_internal_transactions(conta)
-    
+def count_sent_recived(accounts, user_to, user_from):
+    '''
+    Calcula a quantidade de vezes que uma conta recebeu e enviou transações.
+    Receber como parâmetro a lista de contas, a lista de user_to e user_from
+    '''
+    sent = []
+    received = []
+    for account in tqdm(accounts): 
+        s = np.count_nonzero(user_to == account)
+        r = np.count_nonzero(user_from == account)
         
-
-write_file("user_account,balance_ether,normal_transactions,internal_transactions")
-
-for conta in tqdm(contas): 
-    ether, normal_transactions, internal_transactions = get_ether_balance(conta), get_normal_transactions(conta), get_internal_transactions(conta)
-
-    linha = f"{conta},{ether},{normal_transactions},{internal_transactions}"
-    write_file(linha)
+        sent.append(s)
+        received.append(r)
     
+    sent, received = pd.Series(sent), pd.Series(received)
+
+    return sent, received
+
+
+def count_contracts(df, accounts):
+    '''
+    Função para calcular a quantidade de contratos que uma conta envia e quantos
+    contratos ela recebe. Para isso a função recebe a base de dados das transações
+    e as contas presentes na base
+    '''
+    contracts_sent = []
+    contracts_received = []
+    
+    for a in tqdm(accounts):
+        
+        c_s = sum(df.value[df.user_from == a] == 0)
+        contracts_sent.append(c_s)
+        
+        c_r = sum(df.value[df.user_to == a] == 0)
+        contracts_received.append(c_r)
+        
+    return contracts_sent, contracts_received
+
+def main():
+    df_transactions = pd.read_csv('dataset_fix_20200810.csv')
+    df = pd.read_csv('accounts_features.csv')
+
+    sent, received = count_sent_recived(df.user_account, df_transactions.user_to, df_transactions.user_from)
+    contracts_sent, contracts_received = count_contracts(df_transactions, df.user_account)
+
+    df['sent'] = sent
+    df['received'] = received
+    df['n_contracts_sent'] = contracts_sent
+    df['n_contracts_received'] = contracts_received
+
+    df.to_csv('accounts_features.csv', index=False)
