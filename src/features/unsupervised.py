878c6c2a3e4bf7b8c3c99523f959ff0bdab7525f
Júlia Almeida Valadares <julia.valadares@luizalabs.com>
2021-11-22 20:59:08 -0300

    feat: adiciona criacao do modelo nao supersionado

---
+++ src/features/unsupervised.py
@@ -0,0 +1,59 @@
+import numpy as np
+from matplotlib import pyplot as plt 
+from sklearn.cluster import KMeans 
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+
+def plot_kmeans(X, y_kmeans, kmeans, title):
+    plt.scatter(X[:,1], X[:,2], c=y_kmeans, s=50, cmap='viridis')
+    plt.grid()
+    plt.xlabel("Balance Ether")
+    plt.ylabel("Total transactions")
+    plt.scatter(kmeans.cluster_centers_[:,1],kmeans.cluster_centers_[:,2], c='red', s=100,)
+    plt.savefig("../../reports/figures/kmeans_unlabeled"+title+".png")
+    plt.close()
+
+def plot_graph(x, y, title):
+    plt.plot(x, y, 'bx-')
+    plt.title(title)
+    plt.show()
+    plt.savefig("../../reports/figures/"+title+".png")
+    plt.close()
+
+#################### Import data 
+data = pd.read_csv("../../data/processed/final_dataset_2020.csv")
+data.drop(columns=["Unnamed: 0"], inplace=True)
+
+data = data[data.classification == 0]
+print("Tamanho dos dados sem labels:", len(data))
+
+#################### Preprocess data 
+X = data.iloc[:, [1,3,4,5,6,7]]
+X = MinMaxScaler(feature_range=(0,1)).fit_transform(X)
+y = data.iloc[:, 8]
+
+################# Elbow method 
+wcss = [] 
+silhouette = []
+for n_cluster in range(2, 11): 
+    kmeans = KMeans(n_clusters = n_cluster, init = 'k-means++', n_init = 10, max_iter = 300)
+    kmeans.fit(X) 
+    y_kmeans = kmeans.predict(X)
+    
+    silhouette_avg = silhouette_score(X, y_kmeans)
+    
+    title = f"cluster_{n_cluster}"
+    plot_kmeans(X, y_kmeans, kmeans, title)
+
+    wcss.append(kmeans.inertia_)
+    print(f"For n_cluster = {n_cluster}, silhouette_score = {silhouette_avg}")
+    silhouette.append(silhouette)
+
+
+plt.plot(range(2, 11), wcss, 'bx-')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.savefig("../../reports/figures/elbow_method.png")
+plt.close()
